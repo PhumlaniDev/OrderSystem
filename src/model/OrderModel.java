4f78fee2082b21c708b3549ab19b6e10e27f10f1
PhumlaniDev <60384943+PhumlaniDev@users.noreply.github.com>
2020-04-23 19:18:38 +0200

    Commit from IntelliJ

--- src/model/OrderModel.java
+++ src/model/OrderModel.java
@@ -42,7 +42,7 @@ public class OrderModel {
         System.out.println("\t\t\t6. Cookies                                                R10");
         System.out.println("\t\t\t7. Ice Cream                                              R13");
         System.out.println("\t\t\t8. Fruit Salad                                            R25");
-        System.out.println("\t\t\t8. Muffin                                                 R15");
+        System.out.println("\t\t\t9. Muffin                                                 R15");
         System.out.println("\t`````````````````````````````````````````````````````````````````````````````````");
     }
 
@@ -54,32 +54,202 @@ public class OrderModel {
 
         switch (choose) {
             case 1:
-                if (choose == 1) {
-                    System.out.println("You Selected Cocktails Sausage");
-                    System.out.println("How many you want to order?: ");
-                    quantity = input.nextInt();
-                    total = total + (quantity * 15);
+                System.out.println("You Selected Cocktails Sausage");
+                System.out.println("How many you want to order?: ");
+                quantity = input.nextInt();
+                total = total + (quantity * 15);
 
-                    System.out.println("You want to order again?");
-                    System.out.println();
-                    System.out.println("Press 1 for YES or 2 for NO");
+                System.out.println("You want to order again?");
+                System.out.println();
+                System.out.println("Press 1 for YES or 2 for NO");
 
-                    again = input.nextInt();
+                again = input.nextInt();
 
-                    if (again == 1) {
-                        Menu();
-                        if (choose == 1){
-                            showSnackMenu();
-                        }
-                    } else {
-                        System.out.println("Total price is: " + total);
+                if (again == 1) {
+                    Menu();
+                    if (choose == 1){
+                        showSnackMenu();
                     }
+                } else {
+                    System.out.println("Total price is: " + total);
                 }
                 break;
 
+            case 2:
+                System.out.println("You Selected Hot chips");
+                System.out.println("How many you want to order?: ");
+                quantity = input.nextInt();
+                total = total + (quantity * 15);
+
+                System.out.println("You want to order again?");
+                System.out.println();
+                System.out.println("Press 1 for YES or 2 for NO");
+
+                again = input.nextInt();
+
+                if (again == 1) {
+                    Menu();
+                    if (choose == 1){
+                        showSnackMenu();
+                    }
+                } else {
+                    System.out.println("Total price is: " + total);
+                }
+                break;
+
+            case 3:
+                System.out.println("You Selected Sandwich");
+                System.out.println("How many you want to order?: ");
+                quantity = input.nextInt();
+                total = total + (quantity * 35);
+
+                System.out.println("You want to order again?");
+                System.out.println();
+                System.out.println("Press 1 for YES or 2 for NO");
+
+                again = input.nextInt();
+
+                if (again == 1) {
+                    Menu();
+                    if (choose == 1){
+                        showSnackMenu();
+                    }
+                } else {
+                    System.out.println("Total price is: " + total);
+                }
+                break;
+
+            case 4:
+                System.out.println("You Selected Sweats or Chocolate");
+                System.out.println("How many you want to order?: ");
+                quantity = input.nextInt();
+                total = total + (quantity * 12);
+
+                System.out.println("You want to order again?");
+                System.out.println();
+                System.out.println("Press 1 for YES or 2 for NO");
+
+                again = input.nextInt();
+
+                if (again == 1) {
+                    Menu();
+                    if (choose == 1){
+                        showSnackMenu();
+                    }
+                } else {
+                    System.out.println("Total price is: " + total);
+                }
+                break;
+
+            case 5:
+                System.out.println("You Selected Peanuts or Cashews  ");
+                System.out.println("How many you want to order?: ");
+                quantity = input.nextInt();
+                total = total + (quantity * 9);
+
+                System.out.println("You want to order again?");
+                System.out.println();
+                System.out.println("Press 1 for YES or 2 for NO");
+
+                again = input.nextInt();
+
+                if (again == 1) {
+                    Menu();
+                    if (choose == 1){
+                        showSnackMenu();
+                    }
+                } else {
+                    System.out.println("Total price is: " + total);
+                }
+                break;
+
+            case 6:
+                System.out.println("You Selected Cookies");
+                System.out.println("How many you want to order?: ");
+                quantity = input.nextInt();
+                total = total + (quantity * 10);
+
+                System.out.println("You want to order again?");
+                System.out.println();
+                System.out.println("Press 1 for YES or 2 for NO");
+
+                again = input.nextInt();
+
+                if (again == 1) {
+                    Menu();
+                    if (choose == 1){
+                        showSnackMenu();
+                    }
+                } else {
+                    System.out.println("Total price is: " + total);
+                }
+                break;
+
+            case 7:
+                System.out.println("You Selected Ice Cream");
+                System.out.println("How many you want to order?: ");
+                quantity = input.nextInt();
+                total = total + (quantity * 13);
+
+                System.out.println("You want to order again?");
+                System.out.println();
+                System.out.println("Press 1 for YES or 2 for NO");
+
+                again = input.nextInt();
+
+                if (again == 1) {
+                    Menu();
+                    if (choose == 1){
+                        showSnackMenu();
+                    }
+                } else {
+                    System.out.println("Total price is: " + total);
+                }
+                break;
+
+            case 8:
+                System.out.println("You Selected Fruit Salad");
+                System.out.println("How many you want to order?: ");
+                quantity = input.nextInt();
+                total = total + (quantity * 25);
+
+                System.out.println("You want to order again?");
+                System.out.println();
+                System.out.println("Press 1 for YES or 2 for NO");
+
+                again = input.nextInt();
+
+                if (again == 1) {
+                    Menu();
+                    if (choose == 1){
+                        showSnackMenu();
+                    }
+                } else {
+                    System.out.println("Total price is: " + total);
+                }
+                break;
+
+            case 9:
+                System.out.println("You Selected Muffin");
+                System.out.println("How many you want to order?: ");
+                quantity = input.nextInt();
+                total = total + (quantity * 15);
+
+                System.out.println("You want to order again?");
+                System.out.println();
+                System.out.println("Press 1 for YES or 2 for NO");
+
+                again = input.nextInt();
+
+                if (again == 1) {
+                    Menu();
+                    if (choose == 1){
+                        showSnackMenu();
+                    }
+                } else {
+                    System.out.println("Total price is: " + total);
+                }
+                break;
         }
-
-
     }
-
 }
